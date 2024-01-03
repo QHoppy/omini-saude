d1f34e5eecf450fba28e73314b3ef348cf305c55
Danilo Kasparian <dan.acceto@gmail.com>
2024-01-02 19:43:10 -0500

    ci: setup pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
@@ -6,5 +8,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
